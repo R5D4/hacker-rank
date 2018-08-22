085dc1d54f9ee1ee162510ba72005dd764e69fa4
R5D4 <ding.randy@gmail.com>
2018-08-22 15:23:34 -0400

    Add first test

---
+++ 2d_array/test/test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const hourglassSum = require('../src/hourglassSum');
+
+describe('hourglassSum', function() {
+	it('should return maximum hourglass sum', function() {
+        const arr = [ [1,1,1,0,0,0],
+                      [0,1,0,0,0,0],
+                      [1,1,1,0,0,0],
+                      [0,0,2,4,4,0],
+                      [0,0,0,2,0,0],
+                      [0,0,1,2,4,0] ];
+        const maxSum = hourglassSum(arr);
+		expect(maxSum).to.equal(19);
+	});
+});
