8f0703f22080da4b204566fe10ed0f8ddad35a32
R5D4 <ding.randy@gmail.com>
2018-09-09 13:24:32 -0400

    Finish working version of rotate array

--- rotate_array/src/rotate_array.js
+++ rotate_array/src/rotate_array.js
@@ -5,12 +5,14 @@
  */
 function rotLeft(a, d) {
     function rotateOnce(arr) {
+        // create new array where each element is shifted to the left by one position
+        const rotated = arr.map((e, i) => arr[(i + (arr.length + 1)) % arr.length]);
         return rotated;
     }
-
-    let rotatedArray;
-    for(let i = d; i < d; i++) {
-        
+    
+    let rotatedArray = a;
+    for(let i = 0; i < d; i++) {
+        rotatedArray = rotateOnce(rotatedArray);
     }
     return rotatedArray;
 }
