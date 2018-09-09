b17965715503f43cd17d91751ecb0a80c04b2041
R5D4 <ding.randy@gmail.com>
2018-09-09 13:29:01 -0400

    Calculate final array elements directly

--- rotate_array/src/rotate_array.js
+++ rotate_array/src/rotate_array.js
@@ -4,16 +4,8 @@
  * @param {int} d Number of times to rotate left 
  */
 function rotLeft(a, d) {
-    function rotateOnce(arr) {
-        // create new array where each element is shifted to the left by one position
-        const rotated = arr.map((e, i) => arr[(i + (arr.length + 1)) % arr.length]);
-        return rotated;
-    }
-    
-    let rotatedArray = a;
-    for(let i = 0; i < d; i++) {
-        rotatedArray = rotateOnce(rotatedArray);
-    }
+    // calculate new indices of each element and map into new array
+    const rotatedArray = a.map((e, i) => a[(i + (a.length + d)) % a.length]);    
     return rotatedArray;
 }
 
