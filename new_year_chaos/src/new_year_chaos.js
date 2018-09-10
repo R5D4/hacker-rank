e1d5179daf8c75b25ec5526eb138bfb603e8eb5c
R5D4 <ding.randy@gmail.com>
2018-09-10 19:08:55 -0400

    Finish new year chaos implementation and tests.

--- new_year_chaos/src/new_year_chaos.js
+++ new_year_chaos/src/new_year_chaos.js
@@ -1,5 +1,24 @@
+/**
+ * Return minimum number of movies to get queue into current configuration.
+ * Return 'Too chaotic' if no illegal configuration.
+ * @param {array} q Array of integers representing initial position (start from 1)
+ */
 function minimumBribes(q) {
-    const bribes = 0;
+    let bribes = 0;
+    // quit before the last position
+    for (let i = 0; i < q.length - 1; i++) {
+        const current = q[i];
+        const position = i + 1;
+        const right = q[i + 1];
+        if (current > position + 2) {return 'Too chaotic';}
+
+        // Calculate number of bribes
+        if (current > position) {
+            bribes += (current - position);
+        } else if (current > right) {
+            bribes++;
+        }
+    }
     return bribes;
 }
 
--- new_year_chaos/test/test.js
+++ new_year_chaos/test/test.js
@@ -7,4 +7,28 @@ describe('minimumBribes', () => {
         const bribes = minimumBribes(q);
         expect(bribes).to.equal(3);
     });
+
+    it('should output 4 for this input', () => {
+        const q = [2, 1, 5, 4, 3];
+        const bribes = minimumBribes(q);
+        expect(bribes).to.equal(4);
+    });
+
+    it('should output 6 for this input', () => {
+        const q = [2, 4, 5, 3, 1];
+        const bribes = minimumBribes(q);
+        expect(bribes).to.equal(6);
+    });
+
+    it('should output 4 for this input', () => {
+        const q = [2, 4, 3, 1, 5];
+        const bribes = minimumBribes(q);
+        expect(bribes).to.equal(4);
+    });
+
+    it('should output "Too chaotic" for illegal configuration', () => {
+        const q = [2, 5, 1, 3, 4];
+        const bribes = minimumBribes(q);
+        expect(bribes).to.equal('Too chaotic');
+    })
 });
