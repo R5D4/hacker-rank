7a9abf978d84ce981d5276c91c300f7ed20aaf43
R5D4 <ding.randy@gmail.com>
2018-09-12 14:13:24 -0400

    Clean up code

--- new_year_chaos/src/new_year_chaos.js
+++ new_year_chaos/src/new_year_chaos.js
@@ -7,15 +7,17 @@ function minimumBribes(q) {
     let bribes = 0;
     // quit before the last position
     for (let i = 0; i < q.length - 1; i++) {
-        const current = q[i];
-        const position = i + 1;
-        const right = q[i + 1];
-        if (current > position + 2) {return 'Too chaotic';}
+        const initPos = q[i];
+        const currentPos = i + 1;
+        const nextInitPos = q[i + 1];
+
+        // no one can move up more than two positions, invalid configuration
+        if (initPos > currentPos + 2) {return 'Too chaotic';}
 
         // Calculate number of bribes
-        if (current > position) {
-            bribes += (current - position);
-        } else if (current > right) {
+        if (initPos > currentPos) {
+            bribes += (initPos - currentPos);
+        } else if (initPos > nextInitPos) {
             bribes++;
         }
     }
