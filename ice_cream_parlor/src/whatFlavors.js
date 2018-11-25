0c35206582381bc7d95e3412367509423dfc301c
R5D4 <ding.randy@gmail.com>
2018-11-25 15:00:11 -0500

    Set up ice cream parlor project

---
+++ ice_cream_parlor/src/whatFlavors.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {array} cost 
+ * @param {int} money 
+ * @returns {string} id of the two flavours of ice cream, space separated
+ */
+function whatFlavors(cost, money) {
+    
+    return '';
+}
+
+module.exports = whatFlavors;
---
+++ ice_cream_parlor/test/test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const whatFlavors = require('../src/whatFlavors');
+
+describe('whatFlavors', () => {
+    const params = [
+        {cost: [1, 4, 5, 3, 2], money: 5, expected: '1 4'},
+        {cost: [2, 2, 4, 3], money: 4, expected: '1 2'},
+    ];
+    params.forEach(param => {
+        it(`outputs correct flavor IDs for cost: [${param.cost}] and ${param.money} money`, () => {
+            const flavorIDs = whatFlavors(param.cost, param.money);
+            expect(flavorIDs).to.equal(param.expected);
+        });
+    });
+});
