a0e3d02927c880be2ae9485402011d02270fdb53
R5D4 <ding.randy@gmail.com>
2018-11-28 10:47:59 -0500

    one-pass hash table version time O(n)

--- ice_cream_parlor/src/whatFlavors.js
+++ ice_cream_parlor/src/whatFlavors.js
@@ -6,15 +6,16 @@
  */
 function whatFlavors(cost, money) {
     let chosenFlavors = '';
-    for (let i = 0; i < cost.length - 1; i++) {
-        const otherFlavors = cost.slice(i + 1);
-        const id1 = i;
-        // search for (money - cost of first flavor) in the otherFlavors array
-        const id2 = otherFlavors.indexOf(money - cost[id1]);
-        if (id2 >= 0) { // found a match
-            chosenFlavors = `${id1 + 1} ${id1 + 1 + id2 + 1}`; // convert to 1-base index
+    const hTable = {};
+    for (let i = 0; i < cost.length; i++) {
+        const id = i;
+        // search for (money - cost of first flavor) in the hash table
+        const id2 = hTable[money - cost[id]];
+        if (id2 !== undefined) { // found a match
+            chosenFlavors = `${id2 + 1} ${id + 1}`; // convert to 1-base index
             break;
         }
+        hTable[cost[id]] = id;
     };
     return chosenFlavors;
 }
--- ice_cream_parlor/test/test.js
+++ ice_cream_parlor/test/test.js
@@ -5,6 +5,7 @@ describe('whatFlavors', () => {
     const params = [
         {cost: [1, 4, 5, 3, 2], money: 4, expected: '1 4'},
         {cost: [2, 2, 4, 3], money: 4, expected: '1 2'},
+        {cost: [1, 2, 4, 3], money: 7, expected: '3 4'},
     ];
     params.forEach(param => {
         it(`outputs correct flavor IDs for cost: [${param.cost}] and ${param.money} money`, () => {
