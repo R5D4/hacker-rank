d8b02cfb3f8151ecabce0cdfd167cd762f2b9029
R5D4 <ding.randy@gmail.com>
2019-01-28 18:05:30 -0500

    Faster algo curtesy of internet

--- minimumSwaps/src/minimumSwaps.js
+++ minimumSwaps/src/minimumSwaps.js
@@ -1,35 +1,13 @@
 function minimumSwaps (arr) {
-    let swaps = 0;
-
-    // create graph from array
-    const graph = {};
-    arr.forEach((v, i) => {
-        graph[v - 1] = i;
-    });
-
-    // find connected components
-    const components = [];
-    while (Object.entries(graph).length !== 0) {
-        let node = Object.keys(graph)[0];
-        const component = [node];
-        components.push(component);
-        let next = graph[node];
-        delete graph[node];
-        node = next;
-        
-        while (graph[node] !== undefined) {
-            component.push(node);
-            next = graph[node];
-            delete graph[node];
-            node = next;
+    let swaps = 0
+    for (let i = 0, n = arr.length; i < n; i++) {
+        while (arr[i] !== i + 1) {
+            arr[i] = [arr[arr[i]-1], arr[arr[i]-1] = arr[i]][0]
+            if (!arr[i]) break
+            swaps++
         }
-
-        // found all nodes in a connected component
-        // calculate # of swaps needed to sort this component
-        swaps += component.length - 1;
     }
-    
-    return swaps;
+    return swaps
 }
 
 exports.minimumSwaps = minimumSwaps;
