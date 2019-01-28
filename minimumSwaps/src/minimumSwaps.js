d404e0ee3ece1228a8d67b108d28a20d29fc4c8e
R5D4 <ding.randy@gmail.com>
2019-01-28 18:06:18 -0500

    add semicolons

--- minimumSwaps/src/minimumSwaps.js
+++ minimumSwaps/src/minimumSwaps.js
@@ -1,13 +1,13 @@
 function minimumSwaps (arr) {
-    let swaps = 0
+    let swaps = 0;
     for (let i = 0, n = arr.length; i < n; i++) {
         while (arr[i] !== i + 1) {
-            arr[i] = [arr[arr[i]-1], arr[arr[i]-1] = arr[i]][0]
-            if (!arr[i]) break
-            swaps++
+            arr[i] = [arr[arr[i]-1], arr[arr[i]-1] = arr[i]][0];
+            if (!arr[i]) break;
+            swaps++;
         }
     }
-    return swaps
+    return swaps;
 }
 
 exports.minimumSwaps = minimumSwaps;
