20130938ae6b477117ef52b426a2b0ba50ac7dfc
R5D4 <ding.randy@gmail.com>
2019-01-30 15:14:51 -0500

    implemented faster algorithm for array manipulation

--- arrayManipulation/src/arrayManipulation.js
+++ arrayManipulation/src/arrayManipulation.js
@@ -2,20 +2,20 @@ function arrayManipulation(n, queries) {
     // create zero-filled array of length n
     const array = new Array(n).fill(0);
 
-    // apply each operation to array
+    // apply each operation to array by only recording changes at edges of range
     queries.forEach(([a, b, k]) => {
-        // add k to array between indices a and b inclusive
-        // a and b are for 1-indexed array so subtract 1 from each
-        for (let i = a - 1; i <= b - 1; i++) {
-            array[i] += k;
-        }
+        // record change at start of range
+        array[a - 1] += k;
+        // record change at end of range if possible
+        if (b < n) array[b] -= k;
     });
 
     // determine maximum value in array
     let max = -Infinity;
-    let pos = n;
-    while (pos--) {
-        max = array[pos] > max ? array[pos] : max;
+    let current = 0;
+    for (let i = 0; i < n; i++) {
+        current += array[i];
+        max = current > max ? current : max;
     }
 
     return max;
