02b8cf1c2543b78403881f98a9d3c74572ed8783
R5D4 <ding.randy@gmail.com>
2019-01-31 15:58:42 -0500

    Sherlock and anagrams: cache previous results to speed up the algo

--- sherlockAndAnagrams/src/sherlockAndAnagrams.js
+++ sherlockAndAnagrams/src/sherlockAndAnagrams.js
@@ -4,16 +4,31 @@ function sherlockAndAnagrams (s) {
      * Assume s1 and s2 have same length
      * @param {string} s1 
      * @param {string} s2 
+     * @param {obj} cache Saved results from previous runs
      */ 
-    function isAnagram (s1, s2) {
+    function isAnagram (s1, s2, cache) {
+        // retrieve frequency maps from cache if possible
+        let charCountS1 = {}, charCountS2 = {};
+
         // key = character, value = frequency
-        const charCountS1 = {}, charCountS2 = {};
-        for (let c of s1) {
-            charCountS1[c] = charCountS1[c] + 1 || 1;
+        if (cache[s1]) {
+            charCountS1 = cache[s1];
+        } else {
+            for (let c of s1) {
+                charCountS1[c] = charCountS1[c] + 1 || 1;
+            }
+            cache[s1] = charCountS1;
         }
-        for (let c of s2) {
-            charCountS2[c] = charCountS2[c] + 1 || 1;
+
+        if (cache[s2]) {
+            charCountS2 = cache[s2];
+        } else {
+            for (let c of s2) {
+                charCountS2[c] = charCountS2[c] + 1 || 1;
+            }
+            cache[s2] = charCountS2;
         }
+
         for (let c of s1) {
             if (charCountS1[c] !== charCountS2[c]) {
                 return false;
@@ -23,12 +38,13 @@ function sherlockAndAnagrams (s) {
     }
 
     let pairs = 0;
+    let cache = {};
     for (let start = 0; start < s.length - 1; start++) {
         for (let len = 1; len < s.length - start; len++) {
             const s1 = s.substring(start, start + len);
             for (let s2Start = start + 1; s2Start < s.length - len + 1; s2Start++) {
                 const s2 = s.substring(s2Start, s2Start + len);
-                if (isAnagram(s1, s2)) pairs++;
+                if (isAnagram(s1, s2, cache)) pairs++;
             }
         }
     }
