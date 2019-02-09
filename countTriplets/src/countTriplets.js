5a4fba4ddddc31fb3b7f3e343eff573fb6662398
R5D4 <ding.randy@gmail.com>
2019-02-09 14:13:00 -0500

    Counting Triplets: finished faster algorithm

--- countTriplets/src/countTriplets.js
+++ countTriplets/src/countTriplets.js
@@ -1,39 +1,21 @@
 function countTriplets(arr, r) {
-    // create map: key = number, value = [index1, index2, ...]
     const map = {};
-    arr.forEach((cur, idx) => {
-        if (map[cur] !== undefined) {
-            map[cur].push(idx);
-        } else {
-            map[cur] = [idx];
-        }
-    });
-
-    // find triplets
-    let pairs = 0;
-    for (let i = 0; i < arr.length - 2; i++) {
-        // remove current element from map
-        const cur = arr[i];
-        const iArr = map[cur];
-        iArr.splice(iArr.indexOf(i), 1);
-        // calculate other two numbers in progression
-        const second = cur * r;
+    let count = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const num = arr[i];
+        const second = arr[i] * r;
         const third = second * r;
-        // find number of valid pairs of second and third values
-        if (map[second] !== undefined && map[second].length > 0) {
-            map[second].forEach(id2 => {
-                let n2 = 0, n3 = 0; // number of valid pairs of second and third values
-                if (id2 > i) {
-                    n2++;
-                    if (map[third] !== undefined && map[third].length > 0) {
-                        n3 = map[third].reduce((acc, id3) => acc + +(id3 > id2), 0);
-                    }
-                }
-                pairs += n2 * n3;
-            });
+        const keyPair = `${third},${second}`;
+        const keySingle = `${second}`;
+        if (map[keyPair] > 0) {
+            count += map[keyPair];
         }
+        if (map[keySingle] > 0) {
+            map[`${second},${num}`] = map[`${second},${num}`] + map[keySingle] || map[keySingle];
+        }
+        map[num] = map[num] + 1 || 1;
     }
-    return pairs;
+    return count;
 }
 
 exports.countTriplets = countTriplets;
