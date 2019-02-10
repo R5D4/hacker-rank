b6ca229f9a697d9a211940e7550e231863e3caf7
R5D4 <ding.randy@gmail.com>
2019-02-10 15:04:04 -0500

    freqQuery: Add more test cases

--- frequencyQuery/test/test.js
+++ frequencyQuery/test/test.js
@@ -7,4 +7,16 @@ describe('freqQuery', () => {
         const answers = freqQuery(queries);
         expect(answers).to.deep.equal([0, 1]);
     });
+
+    it('should return [0, 1] #2', () => {
+        const queries = [[3, 4], [2, 1003], [1, 16], [3, 1]];
+        const answers = freqQuery(queries);
+        expect(answers).to.deep.equal([0, 1]);
+    });
+
+    it('should return [0, 1, 1]', () => {
+        const queries = [[1, 3], [2, 3], [3, 2], [1, 4], [1, 5], [1, 5], [1, 4], [3, 2], [2, 4], [3, 2]];
+        const answers = freqQuery(queries);
+        expect(answers).to.deep.equal([0, 1, 1]);
+    });
 });
