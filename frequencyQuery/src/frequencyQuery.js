9f1e8032e477b7124a640f3d4525bfbbbe35327c
R5D4 <ding.randy@gmail.com>
2019-02-12 16:45:50 -0500

    freqQuery: Finished faster implementation

--- frequencyQuery/src/frequencyQuery.js
+++ frequencyQuery/src/frequencyQuery.js
@@ -1,23 +1,51 @@
 function freqQuery(queries) {
+    class countMap {
+        constructor () {
+            this.map = {};
+        }
+
+        get (data) {
+            return this.map[data];
+        }
+        
+        add (data) {
+            if (data === undefined)  return;
+            if (this.map[data] > 0) {
+                this.map[data]++;
+            } else if (this.map[data] === undefined) {
+                this.map[data] = 1;
+            }
+        }
+
+        sub (data) {
+            if (data === undefined)  return;
+            if (this.map[data] > 1) {
+                this.map[data]--;
+            } else if (this.map[data] === 1) {
+                // map[data] will never remain 0; will change to undefined
+                delete this.map[data];
+            }
+        }
+    }
+
+    const valueCount = new countMap();
+    const freqCount = new countMap();
     const answers = [];
-    const map = {};
+
     queries.forEach(([op, data]) => {
         switch (op) {
             case 1:
-                if (map[data] !== undefined) { 
-                    map[data]++; 
-                } else {
-                    map[data] = 1;
-                }
+                freqCount.sub(valueCount.get(data));
+                valueCount.add(data);
+                freqCount.add(valueCount.get(data));
                 break;
             case 2:
-                if (map[data] > 0) {
-                    map[data]--;
-                }
+                freqCount.sub(valueCount.get(data));
+                valueCount.sub(data);
+                freqCount.add(valueCount.get(data));
                 break;
             case 3:
-                const ans = Object.values(map).some(e => e === data);
-                answers.push(+ans);
+                answers.push(+!!freqCount.get(data));
                 break;
             default:
                 break;
