1cbba130c72c5f389180908fcf945ae1d1f84527
R5D4 <ding.randy@gmail.com>
2019-02-13 17:31:32 -0500

    activityNotification: counting sort. Still too slow.

--- activityNotifications/src/activityNotifications.js
+++ activityNotifications/src/activityNotifications.js
@@ -1,10 +1,28 @@
 function activityNotifications (expenditure, d) {
+    /**
+     * Counting sort
+     * @param {array} arr 
+     */
+    function sort (arr) {
+        const count = new Array(201).fill(0);
+        const output = [];
+        arr.forEach(e => count[e]++);
+        for (let i = 1; i < count.length; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+        for (let i = arr.length - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+        }
+        return output;
+    }
+
     function getMedian (arr) {
-        arr.sort((a, b) => a - b);
-        if (arr.length % 2 === 0) { 
-            return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2; 
+        const sorted = sort(arr);
+        if (sorted.length % 2 === 0) { 
+            return (sorted[sorted.length / 2 - 1] + sorted[sorted.length / 2]) / 2; 
         } else {
-            return arr[Math.floor(arr.length / 2)];
+            return sorted[Math.floor(sorted.length / 2)];
         }
     }
 
