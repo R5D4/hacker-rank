e8fffc497ed8f81dffa8e4bce266fcaf2468d233
R5D4 <ding.randy@gmail.com>
2019-03-01 16:41:36 -0500

    commonChild (LCS): implemented memoized algorithm. 6/15 test cases failed

--- commonChild/src/commonChild.js
+++ commonChild/src/commonChild.js
@@ -1,13 +1,19 @@
 function commonChild (s1, s2) {
-    let len;
-    if (s1.length === 0 || s2.length === 0) {
-        len = 0;
-    } else if (s1[s1.length - 1] === s2[s2.length -1]) {
-        len = commonChild(s1.substring(0, s1.length - 1), s2.substring(0, s2.length - 1)) + 1;
-    } else {
-        len = Math.max(commonChild(s1, s2.substring(0, s2.length - 1)), commonChild(s1.substring(0, s1.length - 1), s2));
+    const memo = {};
+    function LCS (ss1, ss2) {
+        let len;
+        if (memo[`${ss1.length},${ss2.length}`]) return memo[`${ss1.length},${ss2.length}`];
+        if (ss1.length === 0 || ss2.length === 0) {
+            len = 0;
+        } else if (ss1[ss1.length - 1] === ss2[ss2.length -1]) {
+            len = LCS(ss1.substring(0, ss1.length - 1), ss2.substring(0, ss2.length - 1)) + 1;
+        } else {
+            len = Math.max(LCS(ss1, ss2.substring(0, ss2.length - 1)), LCS(ss1.substring(0, ss1.length - 1), ss2));
+        }
+        memo[`${ss1.length},${ss2.length}`] = len;
+        return len;
     }
-    return len;
+    return LCS(s1, s2);
 }
 
 exports.commonChild = commonChild;
